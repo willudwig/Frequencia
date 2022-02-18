60c61f7e2449866d4f6e51d740e120d7999f081d
William Ludwig <acesso.william@gmail.com>
2022-02-18 16:29:09 -0300

    Add project files.

---
+++ Frequencia.ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Frequencia.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("quantos serão lidos");
+            int num = int.Parse(Console.ReadLine());
+            
+
+            for (int i = 0; i < num; i++)
+            {
+                Console.WriteLine("entre com o valor: ");
+                Console.ReadLine();
+            }
+            if ()
+            {
+
+            }
+           
+
+        }
+    }
+}
