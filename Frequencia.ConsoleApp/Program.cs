59142fc98c569bb43f13e28021f72ffa1f827e58
William Ludwig <acesso.william@gmail.com>
2022-02-24 09:10:41 -0300

    feito

--- Frequencia.ConsoleApp/Program.cs
+++ Frequencia.ConsoleApp/Program.cs
@@ -6,21 +6,31 @@ namespace Frequencia.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("quantos serão lidos");
-            int num = int.Parse(Console.ReadLine());
+            int[] numeros = new int[] { 8, 10, 8, 260, 4, 10, 10, 10, 5, 260 };
+
+            int i = 0;
+            int j = 0;
+            int contador = 0;
+
+            Array.Sort(numeros);
+
+            while (i < numeros.Length)
+            {
+                while (j < numeros.Length)
+                {
+                    if (numeros[j] == numeros[i])
+                    {
+                        contador++;
+                    }
+                    j++;
+                }
+                Console.WriteLine("O número {0} aparece {1} vez(es)", numeros[i], contador);
+                i += contador;
+                j = i;
+                contador = 0;
+            }
             
-
-            for (int i = 0; i < num; i++)
-            {
-                Console.WriteLine("entre com o valor: ");
-                Console.ReadLine();
-            }
-            if ()
-            {
-
-            }
-           
-
+            Console.ReadKey();
         }
     }
 }
